f183d74f24ad9be5d99f33ba2415a3f344623a4a
Francis Kikulwe <francissteven1@gmail.com>
2023-03-13 14:43:51 +0400

    add form to submit city names

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 function App() {
+  const handleSubmit = (e) => {
+  };
   return (
-    <div>
-      <h1 className="text-3xl">Compare Cost of Living</h1>
+    <div className="p-4">
+      <h1 className="text-3xl mb-4">Compare Cost of Living</h1>
+      <form onSubmit={handleSubmit} className="">
+        <label 
+            className="text-ml font-bold"
+            htmlFor="city1">City 1</label>
+        <input 
+            className="ml-2 border-2 border-gray-500 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+            name="city1" id="city1" type="text" placeholder="Enter city name" />
+        <label
+            className="ml-2 text-ml font-bold" 
+            htmlFor="city2">City 2</label>
+        <input
+            className="ml-2 border-2 border-gray-500 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+            name="city2" id="city2" type="text" placeholder="Enter city name" />
+        <button 
+            className="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            type="submit">Compare</button>
+      </form>
     </div>
   );
 }
