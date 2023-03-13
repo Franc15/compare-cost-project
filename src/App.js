a60f811d42ed4e85e318d3e29b35969fe2dd9856
Francis Kikulwe <francissteven1@gmail.com>
2023-03-13 15:15:20 +0400

    fetch and display  capital cities from api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import React, { useState } from 'react';
+
 function App() {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+
   const handleSubmit = (e) => {
+    e.preventDefault();
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'd66f3e0275msh6e1eb0655795b8fp1dfa31jsn4e5dfbdd171e',
+        'X-RapidAPI-Host': 'country-facts.p.rapidapi.com'
+      }
+    };
+    
+    fetch('https://country-facts.p.rapidapi.com/all', options)
+      .then(response => response.json())
+      .then(response => setData(response))
+      .catch(err => setError(err));
+
+    for (let i = 0; i < data.length; i++) {
+      console.log(data[i].capital[0] + ', ' + data[i].name['common']);
+    }
   };
   return (
     <div className="p-4">
@@ -21,6 +43,9 @@ function App() {
             className="ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             type="submit">Compare</button>
       </form>
+      <div className="mt-4">
+        {error && <p className="text-red-500">{error}</p>}
+      </div>
     </div>
   );
 }
